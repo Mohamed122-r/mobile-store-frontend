aba588710502ec12ac4436da2335f7ef4c1833b0
Mohamed122-r <anskajamal@gmail.com>
2025-12-07 11:32:24 +0200

    Update Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,14 +1,12 @@
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">
-        {children}
-      </main>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
