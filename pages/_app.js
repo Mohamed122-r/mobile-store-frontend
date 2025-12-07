c757c4ff8ee65d31bcf3aa680b168da908b266f7
Mohamed122-r <anskajamal@gmail.com>
2025-12-07 11:09:54 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,19 @@
 
+import '@/styles/globals.css'
+import { Toaster } from 'react-hot-toast'
+import { CartProvider } from '@/lib/context/CartContext'
+import { AuthProvider } from '@/lib/context/AuthContext'
+import Layout from '@/components/layout/Layout'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Layout>
+          <Component {...pageProps} />
+          <Toaster position="top-center" />
+        </Layout>
+      </CartProvider>
+    </AuthProvider>
+  )
+}
